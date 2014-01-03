87e231a7e9765d315f05b938e956c9b477a227c4
Hiroaki KAWAI <kawai@iij.ad.jp>
2014-01-04 00:34:37 +0900

    add all bundled db driver dialect classes
    and wrap only devined dbapi methods

--- sqlalchemy_gevent/__init__.py
+++ sqlalchemy_gevent/__init__.py
@@ -15,75 +15,81 @@ class FuncProxy(object):
 
 class Proxy(object):
 	_inner = None
-	_threadpool = None
+	_context = None
 	def __getattr__(self, name):
 		obj = getattr(self._inner, name)
-		if callable(obj) and not name.endswith("Error"):
-			return FuncProxy(obj, self._threadpool)
+		if name in self._context.get("methods",()):
+			threadpool = self._context.get("threadpool", gevent.get_hub().threadpool)
+			return FuncProxy(obj, threadpool)
 		else:
 			return obj
 
 class ConnectionProxy(Proxy):
 	def cursor(self):
+		threadpool = self._context.get("threadpool", gevent.get_hub().threadpool)
+		methods = ("callproc", "close", "execute", "executemany",
+			"fetchone", "fetchmany", "fetchall", "nextset", "setinputsizes", "setoutputsize")
 		return type("CursorProxy", (Proxy,), {
-			"_inner": self._threadpool.apply(self._inner.cursor, None, None),
-			"_threadpool": self._threadpool })()
+			"_inner": threadpool.apply(self._inner.cursor, None, None),
+			"_context": dict(self._context.items()+[("methods", methods),]) })()
 
 class DbapiProxy(Proxy):
 	def connect(self, *args, **kwargs):
+		threadpool = self._context.get("threadpool", gevent.get_hub().threadpool)
+		if self._context.get("single_thread_connection"):
+			threadpool = gevent.threadpool.ThreadPool(1)
+		methods = ("close", "commit", "rollback", "cursor")
 		return type("ConnectionProxy", (ConnectionProxy,), {
-			"_inner": self._threadpool.apply(self._inner.connect, args, kwargs),
-			"_threadpool": self._threadpool })()
+			"_inner": threadpool.apply(self._inner.connect, args, kwargs),
+			"_context": dict(self._context.items()+[("methods", methods), ("threadpool", threadpool)]) })()
 
 class ProxyDialect(default.DefaultDialect):
 	_inner = None
-	_threadpool = None
+	_context = None
 	
 	@classmethod
 	def dbapi(cls):
 		return type("DbapiProxy", (DbapiProxy,), {
 			"_inner": cls._inner.dbapi(),
-			"_threadpool": cls._threadpool })()
+			"_context": cls._context })()
 
-def dialect_maker(db, driver=None, threadpool=None):
+def dialect_name(*args):
+	return "".join([s[0].upper()+s[1:] for s in args if s])+"Dialect"
+
+def dialect_maker(db, driver):
+	class_name = dialect_name(db, driver)
 	if driver is None:
-		modname = "sqlalchemy.dialects.%s.base" % db
-	else:
-		modname = "sqlalchemy.dialects.%s.%s" % (db, driver)
+		driver = "base"
 	
-	dialect = importlib.import_module(modname).dialect
+	dialect = importlib.import_module("sqlalchemy.dialects.%s.%s" % (db, driver)).dialect
 	
-	if threadpool is None:
-		threadpool = gevent.get_hub().threadpool
-	return type("GeventDialect_%s" % modname.replace(".","_"),
+	context = {}
+	if db == "sqlite": # pysqlite dbapi connection requires single threaded
+		context["single_thread_connection"] = True
+	
+	return type(class_name,
 		(ProxyDialect, dialect), {
 		"_inner": dialect,
-		"_threadpool": threadpool })
+		"_context": context })
 
-SqliteDialect = dialect_maker("sqlite", None, gevent.threadpool.ThreadPool(1))
-MysqlDialect = dialect_maker("mysql")
-MysqlCymysqlDialect = dialect_maker("mysql", "cymysql")
-MysqlGaerdbmsDialect = dialect_maker("mysql", "gaerdbms")
-MysqlMysqlConnectorDialect = dialect_maker("mysql", "mysqlconnector")
-MysqlMysqldbDialect = dialect_maker("mysql", "mysqldb")
-MysqlOursqlDialect = dialect_maker("mysql", "oursql")
-MysqlPymysqlDialect = dialect_maker("mysql", "pymysql")
-MysqlPyodbcDialect = dialect_maker("mysql", "pyodbc")
-MysqlZxjdbcDialect = dialect_maker("mysql", "zxjdbc")
-PostgresqlDialect = dialect_maker("postgresql")
-PostgresqlPsycopg2Dialect = dialect_maker("postgresql", "psycopg2")
-PostgresqlPg8000Dialect = dialect_maker("postgresql", "pg8000")
-PostgresqlPypostgresqlDialect = dialect_maker("postgresql", "pypostgresql")
-PostgresqlZxjdbcDialect = dialect_maker("postgresql", "zxjdbc")
-FirebirdDialect = dialect_maker("firebird")
-FirebirdKinterbasdbDialect = dialect_maker("firebird", "kinterbasdb")
-FirebirdFdbDialect = dialect_maker("firebird", "fdb")
-DrizzleDialect = dialect_maker("drizzle")
-DrizzleMysqldbDialect = dialect_maker("drizzle", "mysqldb")
-
+bundled_drivers = {
+	"drizzle":"mysqldb".split(),
+	"firebird":"kinterbasdb fdb".split(),
+	"mssql":"pyodbc adodbapi pymssql zxjdbc mxodbc".split(),
+	"mysql":"mysqldb oursql pyodbc zxjdbc mysqlconnector pymysql gaerdbms cymysql".split(),
+	"oracle":"cx_oracle zxjdbc".split(),
+	"postgresql":"psycopg2 pg8000 pypostgresql zxjdbc".split(),
+	"sqlite":"pysqlite".split(),
+	"sybase":"pysybase pyodbc".split()
+	}
+for db, drivers in bundled_drivers.items():
+	globals()[dialect_name(db)] = dialect_maker(db, None)
+	for driver in drivers:
+		globals()[dialect_name(db,driver)] = dialect_maker(db, driver)
 
 def patch_all():
-	registry.register("sqlite", "sqlalchemy_gevent", "SqliteDialect")
-	registry.register("mysql", "sqlalchemy_gevent", "MySQLDialect")
-	registry.register("postgresql", "sqlalchemy_gevent", "PostgresqlDialect")
+	for db, drivers in bundled_drivers.items():
+		registry.register(db, "sqlalchemy_gevent", dialect_name(db))
+		for driver in drivers:
+			registry.register("%s.%s" % (db,driver), "sqlalchemy_gevent", dialect_name(db,driver))
 
