14c6ea4656ede6a2ba0d5d807c02d0c0a21522e9
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2018-02-22 18:52:51 +0900

    use single underscore for visibility

--- sqlalchemy_gevent.py
+++ sqlalchemy_gevent.py
@@ -12,7 +12,7 @@ def call_in_gevent(tp_factory):
 			return func
 		
 		@functools.wraps(func)
-		def proxy(*arg, **kwargs):
+		def proxy(*args, **kwargs):
 			threadpool = tp_factory()
 			return threadpool.apply_e(BaseException, func, args, kwargs)
 		return proxy
@@ -21,38 +21,38 @@ def call_in_gevent(tp_factory):
 def unproxy_call(func):
 	@functools.wraps(func)
 	def wraps(*args, **kwargs):
-		f = lambda x: x.__inner if isinstance(x, Proxy) else x
+		f = lambda x: x._inner if isinstance(x, Proxy) else x
 		args = [f(a) for a in args]
 		kwargs = {k:f(v) for k,v in kwargs.items()}
 		return func(*args, **kwargs)
 	return wraps
 
 class Proxy(object):
-	__dbapi_methods__ = tuple()
+	_dbapi_methods = tuple()
 	
 	def __init__(self, inner, tp_factory=None):
-		self.__inner = inner
-		self.__tp_factory = tp_factory
+		self._inner = inner
+		self._tp_factory = tp_factory
 	
 	def __getattr__(self, name):
-		obj = getattr(self.__inner, name)
-		if name in self.__dbapi_methods__:
-			return call_in_gevent(self.__tp_factory)(obj)
+		obj = getattr(self._inner, name)
+		if name in self._dbapi_methods:
+			return call_in_gevent(self._tp_factory)(obj)
 		elif callable(obj):
-			return call_in_gevent(self.__tp_factory)(unproxy_call(obj))
+			return call_in_gevent(self._tp_factory)(unproxy_call(obj))
 		else:
 			return obj
 
 class CursorProxy(Proxy):
-	__dbapi_methods__ = ("callproc", "close", "execute", "executemany",
+	_dbapi_methods = ("callproc", "close", "execute", "executemany",
 		"fetchone", "fetchmany", "fetchall", "nextset", "setinputsizes", "setoutputsize")
 
 class ConnectionProxy(Proxy):
-	__dbapi_methods__ = ("close", "commit", "rollback", "cursor")
+	_dbapi_methods = ("close", "commit", "rollback", "cursor")
 	
 	def cursor(self):
-		obj = self.__inner.cursor()
-		return CursorProxy(obj, self.__tp_factory)
+		obj = call_in_gevent(self._tp_factory)(self._inner.cursor)()
+		return CursorProxy(obj, self._tp_factory)
 
 single_pool = gevent.threadpool.ThreadPool(1)
 
@@ -68,28 +68,28 @@ def dialect_maker(db, driver):
 	
 	def wrap_connect(func):
 		@functools.wraps(func)
-		def wraps(*args, **kwrags):
-			con = func(*args, **kwargs)
+		def wraps(*args, **kwargs):
+			tp_factory = lambda: gevent.get_hub().threadpool
 			if db == "sqlite": # pysqlite dbapi connection requires single threaded
-				p = ConnectionProxy(con, single)
-			else:
-				p = ConnectionProxy(con, lambda: gevent.get_hub().threadpool)
-			return p
+				tp_factory = lambda: single_pool
+			
+			con = call_in_gevent(tp_factory)(func)(*args, **kwargs)
+			return ConnectionProxy(con, tp_factory)
 		return wraps
 	
 	def wrap_dbapi(func):
 		@functools.wraps(func)
 		def wrap(*args, **kwargs):
-			m = func(*args, **kwrags)
+			m = func(*args, **kwargs)
 			p = Proxy(m)
 			p.connect = wrap_connect(m.connect)
 			return p
 		return wrap
 	
-	d = Proxy(dialect) # d is Dialect clazz
+	attrs = {"_inner": dialect}
 	if hasattr(dialect, "dbapi"):
-		d.dbapi = wrap_dbapi(dialect.dbapi)
-	return d
+		attrs["dbapi"] = wrap_dbapi(dialect.dbapi)
+	return type(class_name, (default.DefaultDialect, Proxy), attrs)
 
 bundled_drivers = {
 	"drizzle":"mysqldb".split(),
